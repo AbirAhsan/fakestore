f9c124107d25016d8850883b2b52806bba0305c8
Abir Ahsan <abirahsan122@gmail.com>
2022-01-05 15:54:39 +0600

    add splash screen with controller

--- lib/views/screens/splash_screen.dart
+++ lib/views/screens/splash_screen.dart
@@ -1,11 +1,15 @@
+import 'package:fakestore/controller/splash_screen_controller.dart';
 import 'package:fakestore/views/variable/variables.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final SplashScreenController splashScreenCtrl =
+        Get.put(SplashScreenController());
     return const Scaffold(
       body: Center(
         child: Text(
